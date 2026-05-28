a255391c54e97d5478e0217fc30a71da071eb216
paesdv <paesm191@gmail.com>
2026-05-28 14:32:22 -0300

    jwt service

--- user-service/src/main/java/com/ecommerce/user_service/core/usecases/LoginUseCase.java
+++ user-service/src/main/java/com/ecommerce/user_service/core/usecases/LoginUseCase.java
@@ -1,23 +1,25 @@
 package com.ecommerce.user_service.core.usecases;
 
 import com.ecommerce.user_service.core.entities.User;
+import com.ecommerce.user_service.core.gateway.JwtService;
 import com.ecommerce.user_service.core.gateway.PasswordEncoder;
 import com.ecommerce.user_service.core.gateway.UserGateway;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class LoginUseCase {
 
     private final UserGateway userGateway;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public LoginUseCase(UserGateway userGateway, PasswordEncoder passwordEncoder){
+    public LoginUseCase(UserGateway userGateway, PasswordEncoder passwordEncoder, JwtService jwtService){
         this.userGateway = userGateway;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
-    public User execute(String email, String password){
+    public String execute(String email, String password){
         User user = userGateway.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+                .orElseThrow(() -> new UserNotFoundException("Usuário não encontrado."));
         if(!passwordEncoder.matches(password, user.getPassword())){
             throw new InvalidPasswordException("Senha inválida.");
         }
