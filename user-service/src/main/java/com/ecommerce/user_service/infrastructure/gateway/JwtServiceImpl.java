dd5b0f3562028a7f98c27a58809c805208541221
paesdv <paesm191@gmail.com>
2026-06-02 18:49:06 -0300

    jwt service

---
+++ user-service/src/main/java/com/ecommerce/user_service/infrastructure/gateway/JwtServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ecommerce.user_service.infrastructure.gateway;
+
+import com.ecommerce.user_service.core.entities.User;
+import com.ecommerce.user_service.core.gateway.JwtService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtServiceImpl implements JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Override
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
